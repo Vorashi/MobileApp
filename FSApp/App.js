cb2b69c8521dc012d7d93d5248c0906ea7639fbe
Matvei <matvejsarov673@yandex.com>
2025-02-20 09:30:07 +0300

    No comment

--- FSApp/App.js
+++ FSApp/App.js
@@ -63,6 +63,7 @@ const App = () => {
         value={filename}
         onChangeText={setFilename}
       />
+     
       <TextInput
         style={styles.input}
         placeholder="Содержимое файла"
